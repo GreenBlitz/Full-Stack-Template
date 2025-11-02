aa48edd45a359076a51aedb126081e61d731519f
YoniKiriaty <yonikiriaty@gmail.com>
2025-11-02 18:15:57 +0200

    Yoni - made the eslint config working and started configing the eslint

--- backend/build.ts
+++ backend/build.ts
@@ -1,14 +1,14 @@
-import { build } from "esbuild";
+import { build } from 'esbuild'
 
 export function createBuildSettings() {
   return {
-    entryPoints: ["src/main.ts"],
-    outfile: "dist/bundle.js",
+    entryPoints: ['src/main.ts'],
+    outfile: 'dist/bundle.js',
     bundle: true,
     plugins: [],
     minify: true,
-    platform: "node",
-    target: ["node16"],
-  } satisfies Parameters<typeof build>[0];
+    platform: 'node',
+    target: ['node16'],
+  } satisfies Parameters<typeof build>[0]
 }
-build(createBuildSettings());
+build(createBuildSettings())
