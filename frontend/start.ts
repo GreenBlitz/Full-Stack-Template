7b06a0dfd3434cde21f4d3f13703d21813f11740
YoniKiriaty <yonikiriaty@gmail.com>
2025-11-09 18:58:54 +0200

    Yoni - bettered the starting of the frontend

--- frontend/start.ts
+++ frontend/start.ts
@@ -4,10 +4,11 @@ import path from "path";
 
 const app = express();
 
-const PORT = process.env.PORT || 443;
-const __dirname = path.dirname(__filename);
+const defaultPort = 443;
+const port = process.env.PORT ?? defaultPort;
+const dirname = path.dirname(__filename);
 
-const distDirectory = path.join(__dirname, "dist");
+const distDirectory = path.join(dirname, "dist");
 const indexHTML = path.join(distDirectory, "index.html");
 
 app.use(express.static(distDirectory));
@@ -16,6 +17,6 @@ app.get(/^(.*)$/, (req, res) => {
   res.sendFile(indexHTML);
 });
 
-app.listen(PORT, () => {
-  console.log(`Production server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Production server running at http://localhost:${port}`);
 });
