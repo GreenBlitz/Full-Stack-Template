99016ef79f03a064258e463a24c1624eb33a2be1
Turbobot <turbobot@vercel.com>
2025-11-12 18:09:03 +0200

    feat(create-turbo): apply official-starter transform

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,13 @@
 // בס"ד
 console.log("Hello World!");
 
+/**
+ * @function skibidi adds an exclamation point
+ * @param name is the name of the person
+ * @returns 
+ * 
+ * 
+ */
+function skibidi(name: string) {
+    return name + "!";
+}
