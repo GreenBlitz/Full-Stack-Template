6dee0299c28951525e7847d32dad956c2d49e449
YoniKiriaty <yonikiriaty@gmail.com>
2025-12-03 18:09:53 +0200

    Yoni - made bit array working

--- apps/template/backend/build.ts
+++ apps/template/backend/build.ts
@@ -35,3 +35,15 @@ const buildedProject = isDev ? buildDev() : build(buildSettings);
 buildedProject.catch((error: unknown) => {
   console.warn(error);
 });
+
+async function bomboclat() {
+  return new Promise<boolean>((resolve, reject) => {
+    resolve(true);
+  });
+}
+
+const isBomboclat = await bomboclat();
+
+console.log(isBomboclat);
+
+
